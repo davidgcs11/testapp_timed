1bbf7179f3e9a4049e5b63793f4f8dfb601bca1d
DavidCabrera <davidgcs11@gmail.com>
2020-02-07 00:19:20 -0500

    not able to make sockets work, so sad

---
+++ lib/models/chat_models.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Message {
+  final String message;
+  final String username;
+
+  Message({
+    @required this.message,
+    @required this.username,
+  });
+}
